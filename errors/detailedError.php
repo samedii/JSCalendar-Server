1f789a6f4198445405519118ae04a624f3e6f30a
direwolf.se <direwolf.se@onecom.(none)>
2014-09-05 14:24:05 +0000

    added a detailed view

--- errors/detailedError.php
+++ errors/detailedError.php
@@ -9,34 +9,28 @@ mysql_select_db('direwolf_se') or die('Could not select database<br/>');
 echo "Selected db successfully<br/>";
 
 // Performing SQL query
-$query = 'SELECT id,errorMsg,url,lineNumber,date FROM JSCalendarErrors';
+$query = 'SELECT * FROM JSCalendarErrors WHERE id=' . $_GET['id'];
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 
-
-// Printing results in HTML
 echo "<table style='border: 1px solid black;'>\n";
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
     echo "\t<tr>\n";
 
-$id = -1;
+foreach ($line as $col_value) {
 
-    foreach ($line as $col_value) {
-if($id == -1) {
-$id = $col_value;
-}
         echo "\t\t<td style='border: 1px solid black;'>$col_value</td>\n";
     }
 
-echo "<td><a href='detailedError.php?id=$id'>Detailed view</a></td>";
-
     echo "\t</tr>\n";
-}
+
 echo "</table>\n";
+}
 
 // Free resultset
 mysql_free_result($result);
 
 // Closing connection
 mysql_close($link);
+
 ?>
