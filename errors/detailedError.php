18bcc9c8846f58e18e55da54a029e378672a30bf
direwolf.se <direwolf.se@onecom.(none)>
2014-09-05 20:19:55 +0000

    Detailed view and json2html should be working

--- errors/detailedError.php
+++ errors/detailedError.php
@@ -1,3 +1,8 @@
+<html>
+<head>
+</head>
+<body>
+
 <?php
 
 // Connecting, selecting database
@@ -9,15 +14,22 @@ mysql_select_db('direwolf_se') or die('Could not select database<br/>');
 echo "Selected db successfully<br/>";
 
 // Performing SQL query
-$query = 'SELECT * FROM JSCalendarErrors WHERE id=' . $_GET['id'];
+$query = 'SELECT state,id,errorMsg,url,lineNumber,date FROM JSCalendarErrors WHERE id=' . $_GET['id'];
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 
 echo "<table style='border: 1px solid black;'>\n";
+
+$state = -1;
+
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
     echo "\t<tr>\n";
 
 foreach ($line as $col_value) {
+	if($state == -1) {
+		$state = $col_value;
+		continue;
+	}
 
         echo "\t\t<td style='border: 1px solid black;'>$col_value</td>\n";
     }
@@ -34,3 +46,19 @@ mysql_free_result($result);
 mysql_close($link);
 
 ?>
+
+<form>
+      <div id="inputcontainer"><textarea id="text" rows="12" style="display:none;"><?php echo $state ?></textarea></div>
+      <div id="output" class="output"></div>
+   </form>
+
+
+<script type="text/javascript" src="/JSCalendar-Server/json2html/scripts/json-min.js"></script>
+<script type="text/javascript" src="/JSCalendar-Server/json2html/scripts/BubbleTooltips.js"></script>
+<script type="text/javascript" src="/JSCalendar-Server/json2html/scripts/parse.js"></script>
+<script>
+doParse();
+</script>
+
+</body>
+</html>
